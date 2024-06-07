92f7e1aa90f619e318f9b0e627130612957ce746
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2024-06-07 11:40:44 +0800

    i dont know git

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -80,7 +80,6 @@
         <button type="submit"
             class="rounded-md bg-indigo-600 px-1.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
     </form>
-
     <br>
 </body>
 <script>
